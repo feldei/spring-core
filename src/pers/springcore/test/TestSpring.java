0e855e8bb12992624cdebfa9ed0cd1b8f175fc58
liushiyang <liushiyang@163.com>
2019-06-02 13:52:57 +0800

    modify

--- src/pers/springcore/test/TestSpring.java
+++ src/pers/springcore/test/TestSpring.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pers.springcore.model.StudentModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: lsy
  * @Date: 2019/6/1 0:51
@@ -15,6 +18,17 @@ public class TestSpring {
     public void testSpring(){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         StudentModel studentModel = (StudentModel)context.getBean("studentModel");
+
         System.out.println(studentModel.toString());
+    }
+
+    @Test
+    public void testMethod() {
+        List<Integer> list = new ArrayList<>(5);
+        list.forEach(integer -> {
+           list.add(1+1);
+        });
+
+        System.out.println(list);
     }
 }
